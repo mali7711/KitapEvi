aff6b355d6917a9125ba29fd787ed3a1441afa34
muhammed380 <muhammed380@BPC07>
2024-02-15 09:15:38 +0300

    Proje dosyası ekle.

---
+++ KitapEvi/Models/KatagoriModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KitapEvi.Models
+{
+    public class KatagoriModel
+    {
+        public int KatagöriId { get; set; }
+        public string KatagöriAdi { get; set; }
+        List<KitapModel> KitapModels=new List<KitapModel>();
+    }
+}
---
+++ KitapEvi/Models/KitapModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KitapEvi.Models
+{
+    public class KitapModel
+    {
+        public int KitapId { get; set; }
+        public string KitapAdi { get; set; }
+        public int SayfaSayısı { get; set; }
+    }
+}
